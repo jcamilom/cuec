106c3c62da157c2b298974e47b370afbaa67ac0d
Camilo Muñoz <jcamilomunozz@gmail.com>
2017-12-18 00:04:28 -0500

    Entrada de lote. Loop

--- placa1.ino
+++ placa1.ino
@@ -11,19 +11,22 @@ static const unsigned char N_9_ASCII = 57;  // Valor en ascii de "9"
 static const unsigned char L_A_ASCII = 65;  // Valor en ascii de "A"
 static const unsigned char L_B_ASCII = 66;  // Valor en ascii de "B"
 static const unsigned char L_C_ASCII = 67;  // Valor en ascii de "C"
-static const unsigned char L_D_ASCII = 68;  // Valor en ascii de "C"
+static const unsigned char L_D_ASCII = 68;  // Valor en ascii de "D"
 
 // Prototipo de funciones
 int powint(int, int);
-void getKey();
+void getLoteNumber();
+void processLoteNumber();
 
 /* Variables globales */
 // Teclado
 bool modoLote = false;                      // Activar cuando se va a ingresar con lote
-unsigned char nDigitos = 0;                 // Contador de digitos ingresados
+unsigned char nDigitos;                 // Contador de digitos ingresados
 unsigned char loteArray[MAX_DIG_LOTE];      // Guarda los dígitos ingresados
-unsigned int lote = 0;                      // Guarda el valor de lote
+unsigned int lote;                      // Guarda el valor de lote
 unsigned char tecla;                        // Tecla ingresada
+// Menu
+unsigned char menu;                     // Menu como máquina de estados
 
 /* Configuración del LCD */
 LiquidCrystal_I2C lcd(0x27, 20, 4);
@@ -69,25 +72,52 @@ void setup() {
     Serial.begin(9600); // Velocidad de comunicacion con la PC
     pinMode(8, INPUT);
     Serialvirt.begin(9600); // Velocidad de comunicacion del otroarduino
+
+    // Variables
+    menu = 0;
+    lote = 0;
+    nDigitos = 0;
 }
 
 void loop() {    
     // Obtiene tecla (si es pulsada)
     tecla = teclado.getKey();
 
-    // Modo lote activado, se pide el lote. Tecla A
-    if (tecla == L_A_ASCII) {
-        getLote();
-    }
-    // Modo lote desactivado. Se empieza a leer área
-    else if (tecla == L_B_ASCII) {
-        lcd.setCursor(6, 2);
-        lcd.print("desactivado");
-    }
-    // POR DEFINIR - borrar???
-    else if (tecla == L_C_ASCII) {
-        lcd.setCursor(6, 0);
-        lcd.print("0            ");
+    if (tecla != NO_KEY) {
+
+        switch(menu) {
+
+            // Inicio
+            case 0:
+                // Tecla A. Modo lote activado
+                if(tecla == L_A_ASCII) {
+                    modoLote = true;                // Activa el modo lote
+                    nDigitos = 0;
+                    lote = 0;
+
+                    // Limpia lote anterior del LCD ó "desactivado"
+                    lcd.setCursor(6, 2);
+                    lcd.print("/          ");
+                    // Limpia el valor anterior de área
+                    lcd.setCursor(6, 0);
+                    lcd.print("----");
+
+                    menu = 1;
+                }
+                // Tecla B. Se inicia getArea sin lote
+                else if(tecla == L_B_ASCII) {
+                    menu = 10;
+                }
+                break;
+
+            // Se pide el valor del lote
+            case 1:
+                getLoteNumber();
+                break;
+
+            default:
+                break;
+        }
     }
 /*
 
@@ -99,67 +129,41 @@ void loop() {
 
 }
 
-/* void lectura_datos() { // funcion lectura del otro arduino
-    Serial.println("Leyendo datos...");
-    delay(5000);
-    //lecturadelotroarduino = Serialvirt.read(); // Lectura de lo que el otro arduino quiere que yo lea   
-    lecturadelotroarduino = 20;
-
-    if (lecturadelotroarduino != 0) {
-        area = lecturadelotroarduino;
-        b = 0;
-        lcd.setCursor(6, 0);
-        lcd.print("             ");
-        delay(1);
-        lcd.setCursor(6, 0);
-        lcd.print(lecturadelotroarduino);
+void getLoteNumber() {
+    // Se entra si se pulsa un número 0-9
+    if ((tecla >= N_0_ASCII) && (tecla <= N_9_ASCII) && (nDigitos < MAX_DIG_LOTE)) {    // Max value 9.999
+        loteArray[nDigitos] = (tecla - N_0_ASCII);      // ASCII -> num, luego se guarda dígito en array. 
+        lcd.setCursor(6 + nDigitos, 2);
+        lcd.print(loteArray[nDigitos]);
+        if (nDigitos != MAX_DIG_LOTE - 1) lcd.print("/");   // Se imprime "-" si no es el último dígito permitido
+        Serial.println(loteArray[nDigitos]);
+        nDigitos++;
     }
-
-    if ((lecturadelotroarduino == 0) && (b == 0)) {
-
+    // Se borra el último dígito, si existe alguno
+    else if ((tecla == L_C_ASCII) && (nDigitos > 0)) {
+        nDigitos--;
+        loteArray[nDigitos] = 0;    // No necesario pero buena práctica
+        lcd.setCursor(6 + nDigitos, 2);
+        lcd.print("/ ");
     }
-} */
+    // Se cancela el modo lote
+    else if (tecla == L_D_ASCII) {
+        modoLote = false;
+        nDigitos = 0;
+        lote = 0;                    
+        processLoteNumber();
 
-void getLote() {
-    modoLote = true;                // Activa el modo lote
-    nDigitos = 0;
-    lote = 0;
-
-    // Limpia lote anterior del LCD ó "desactivado"
-    lcd.setCursor(6, 2);
-    lcd.print("/          ");
-    // Limpia el valor anterior de área
-    lcd.setCursor(6, 0);
-    lcd.print("----");
-
-    // Espera a que se ingrese el valor del lote
-    while (tecla != L_B_ASCII) {
-        tecla = teclado.getKey();
-        // Se entra si se pulsa un número 0-9
-        if ((tecla >= N_0_ASCII) && (tecla <= N_9_ASCII) && (nDigitos < MAX_DIG_LOTE)) {    // Max value 9.999
-            loteArray[nDigitos] = (tecla - N_0_ASCII);      // ASCII -> num, luego se guarda dígito en array. 
-            lcd.setCursor(6 + nDigitos, 2);
-            lcd.print(loteArray[nDigitos]);
-            if (nDigitos != MAX_DIG_LOTE - 1) lcd.print("/");   // Se imprime "-" si no es el último dígito permitido
-            Serial.println(loteArray[nDigitos]);
-            nDigitos++;
-        }
-        // Se borra el último dígito, si existe alguno
-        else if ((tecla == L_C_ASCII) && (nDigitos > 0)) {
-            nDigitos--;
-            loteArray[nDigitos] = 0;    // No necesario pero buena práctica
-            lcd.setCursor(6 + nDigitos, 2);
-            lcd.print("/ ");
-        }
-        // Se cancela el modo lote
-        else if (tecla == L_D_ASCII) {
-            modoLote = false;
-            nDigitos = 0;
-            lote = 0;
-            break;
-        }
+        menu = 0;
     }
+    // Se procesa el valor ingresado
+    else if(tecla == L_B_ASCII) {
+        processLoteNumber();
+        
+        menu = 0;
+    }
+}
 
+void processLoteNumber() {
     // Computa el valor del lote
     for (byte i = nDigitos; i > 0; i--) {
         lote = lote + loteArray[i - 1] * powint(10, nDigitos - i);            
@@ -192,6 +196,27 @@ void getLote() {
         lcd.print("0            ");
     }
 }
+
+/* void lectura_datos() { // funcion lectura del otro arduino
+    Serial.println("Leyendo datos...");
+    delay(5000);
+    //lecturadelotroarduino = Serialvirt.read(); // Lectura de lo que el otro arduino quiere que yo lea   
+    lecturadelotroarduino = 20;
+
+    if (lecturadelotroarduino != 0) {
+        area = lecturadelotroarduino;
+        b = 0;
+        lcd.setCursor(6, 0);
+        lcd.print("             ");
+        delay(1);
+        lcd.setCursor(6, 0);
+        lcd.print(lecturadelotroarduino);
+    }
+
+    if ((lecturadelotroarduino == 0) && (b == 0)) {
+
+    }
+} */
 
 int powint(int x, int y) {
     int val = x;
